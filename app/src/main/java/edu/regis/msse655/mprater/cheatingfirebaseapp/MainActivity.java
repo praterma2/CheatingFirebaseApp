2708497dc224e8024f68496e805fe5e007de9ed0
praterma2 <praterma2@gmail.com>
2017-07-18 16:34:56 -0700

    added indexed keyes

--- app/src/main/java/edu/regis/msse655/mprater/cheatingfirebaseapp/MainActivity.java
+++ app/src/main/java/edu/regis/msse655/mprater/cheatingfirebaseapp/MainActivity.java
@@ -11,7 +11,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-   private Button sendData;
+    private Button sendData;
 
     private Button recData;
 
@@ -25,37 +25,40 @@ public class MainActivity extends AppCompatActivity {
 
         sendData = (Button) findViewById(R.id.button);
 
-
         sendData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Write a message to the database
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
                 DatabaseReference myRef = database.getReference("message");
-                TextView t = (TextView) findViewById(R.id.dBaseView);
-                t.setText(database + "");
-
                 myRef.setValue("Goodbye, World!");
-
-
+                
 
             }
         });
 
         recData = (Button) findViewById(R.id.btn2);
 
+
         recData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int val = 9;
-                int val2 = 10;
+                long val2 = 10;
                 long crtTime= System.currentTimeMillis();
-                String stCrtTime = String.valueOf(crtTime);
-                // Write a message to the database
+                String sttCrtTime = String.valueOf(crtTime) + "t";
+                String stbCrtTime =  String.valueOf(crtTime) + "b";
+                // Write a tone message to the database
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference(stCrtTime);
+                DatabaseReference myRef = database.getReference(sttCrtTime);
+                myRef.setValue(val);
 
-                myRef.setValue(val, val2);
+                //now for the beat
+                DatabaseReference myRef2 = database.getReference(stbCrtTime);
+                myRef2.setValue(val2);
+
+
+
 
             }
         });
